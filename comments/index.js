3dae2618e26b7fa5250e1cb97f98a02def963408
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-04-08 19:29:42 +0100

    Refactored app to use a 'moderation' service that moderates comments and does not show those failing moderating rules (containing the word orange). Refactored query to now listen for a 'CommentUpdated' event, 'dumb' service that only knows to update comments but the logic about how comment is changing resides within comments service.

--- comments/index.js
+++ comments/index.js
@@ -18,7 +18,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
     const comments = commentsByPostsId[req.params.id] || [];
-    comments.push({id: commentId, content })
+    comments.push({id: commentId, content, status: 'pending' })
 
     commentsByPostsId[req.params.id] = comments;
 
@@ -27,6 +27,7 @@ app.post('/posts/:id/comments', async (req, res) => {
         data: {
             id: commentId,
             content,
+            status: 'pending',
             postId: req.params.id
         }
     })
@@ -34,8 +35,30 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log("Received Event:", req.body.type);
+
+    const { type, data } = req.body;
+    if (type === 'CommentModerated') {
+        console.log(data)
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostsId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                content,
+                postId
+            }
+        })
+    }
+
     res.send({});
 });
 
