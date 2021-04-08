86762cc020c08c52220913e06b442f41978681cb
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-04-08 17:43:30 +0100

    Refactored app to use a 'query' service for posts and comments to be served. this allows posts and comments services to fail but allow posts/comments still to be retrieved.
    An event-bus (created manually in this repo) is used to facilitate this.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 
-import CommentCreate from "./CommentCreate";
 
-const CommentList = ({ postId }) => {
-    const [comments, setComments] = useState([]);
-
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    };
-
-    useEffect(() => {
-        fetchComments();
-    }, []);
+const CommentList = ({ comments }) => {
 
     const renderedComments = Object.values(comments).map(comment => {
         return (
